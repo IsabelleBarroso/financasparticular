58156e9cd0e86e76fa5f680edd34aa0bee40d2a1
IsabelleBarroso <121049516+IsabelleBarroso@users.noreply.github.com>
2026-09-16 16:35:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='financas-v1.2';
+const CACHE='financas-v2.1';
 const ARQUIVOS=['./','./index.html','./manifest.json','./icone-192.png','./icone-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ARQUIVOS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
